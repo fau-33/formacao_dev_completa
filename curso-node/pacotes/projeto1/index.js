8d82f44532fa7617dd07b47b1341089839e59940
Flávio Félix <fau_felix30@hotmail.com>
2025-05-23 19:29:20 -0300

    Reenviando o projeto 1

--- curso-node/pacotes/projeto1/index.js
+++ curso-node/pacotes/projeto1/index.js
@@ -1,9 +1,45 @@
 import chalk from "chalk";
+import { DateTime } from "luxon";
+import server from "server";
 
-const azul = (texto) => console.log(chalk.bgBlue(texto));
-const aviso = (texto) => console.log(chalk.bgYellow.bold(texto));
-const erro = (texto) => console.log(chalk.bgRed.bold.italic(texto));
+const { get } = server.router;
 
-azul("Sou um texto");
-aviso("Preciso de atenção");
-erro("Atenção a um erro!");
+function imprimeHora(cidade, hora) {
+  console.log(chalk.bgBlue(`Hora em ${cidade}: ${hora}`));
+}
+
+function getHoraExata(timeZone) {
+  return DateTime.now().setZone(timeZone).toFormat("HH:mm:ss (ZZZZ)");
+}
+
+server({ port: 8080 }, [
+  get("/", (ctx) => "Olá mundo"),
+  get("/roma", () => {
+    const h = getHoraExata("Europe/Rome");
+    imprimeHora("Roma", h);
+    return h;
+  }),
+  get("/luanda", () => {
+    const h = getHoraExata("Africa/Luanda");
+    imprimeHora("Luanda", h);
+    return h;
+  }),
+  get("/seoul", () => {
+    const h = getHoraExata("Asia/Seoul");
+    imprimeHora("Seoul", h);
+    return h;
+  }),
+  get("/cuiaba", () => {
+    const h = getHoraExata("America/Campo_Grande");
+    imprimeHora("Cuiabá", h);
+    return h;
+  }),
+]);
+
+// const azul = (texto) => console.log(chalk.bgBlue(texto));
+// const aviso = (texto) => console.log(chalk.bgYellow.bold(texto));
+// const erro = (texto) => console.log(chalk.bgRed.bold.italic(texto));
+
+// azul("Sou um texto");
+// aviso("Preciso de atenção");
+// erro("Atenção a um erro!");
