7c236662c8b1cc40789b8d832a79fcc2924ef0af
Flávio Félix <fau_felix30@hotmail.com>
2025-05-27 14:38:31 -0300

    Feat: Done crud

--- curso-node/pacotes/projeto3/Banco.js
+++ curso-node/pacotes/projeto3/Banco.js
@@ -1,15 +1,37 @@
-let dadosManipulaveis = []; // formato {nome, id}
+import fs from "node:fs";
+let dadosManipulaveis = _carregarValores(); // formato {nome, id}
+const ultimoIdResgitrado =
+	dadosManipulaveis[dadosManipulaveis.length - 1]?.id ?? -1;
 
+let idGlobal = ultimoIdResgitrado + 1;
+
+function _carregarValores() {
+	const dadosJson = fs.readFileSync("banco.json", "utf8");
+	const dados = JSON.parse(dadosJson);
+	return dados;
+}
+
+export function persistirValores() {
+	const dadosJson = JSON.stringify(dadosManipulaveis, null, 4);
+	fs.writeFileSync("banco.json", dadosJson);
+}
 export function salvar(nome, id) {
-  dadosManipulaveis.push({ nome, id });
+	const indice = dadosManipulaveis.findIndex((obj) => obj.id === id);
+
+	if (indice >= 0) {
+		dadosManipulaveis[indice].nome = nome;
+	} else {
+		dadosManipulaveis.push({ nome, id: idGlobal });
+		idGlobal++;
+	}
 }
 export function deletar(id) {
-  const dadosFiltrados = dadosManipulaveis.filter((dado) => dado.id !== id);
-  dadosManipulaveis = dadosFiltrados;
+	const dadosFiltrados = dadosManipulaveis.filter((dado) => dado.id !== id);
+	dadosManipulaveis = dadosFiltrados;
 }
 export function pegaDados() {
-  const dadosImprimiveis = dadosManipulaveis.map((dado) => {
-    return `${dado.id} - ${dado.nome}`;
-  });
-  return dadosImprimiveis;
+	const dadosImprimiveis = dadosManipulaveis.map((dado) => {
+		return `${dado.id} - ${dado.nome}`;
+	});
+	return dadosImprimiveis;
 }
--- curso-node/pacotes/projeto3/index.js
+++ curso-node/pacotes/projeto3/index.js
@@ -1,15 +1,18 @@
-import { salvar, deletar, pegaDados } from "./Banco.js";
+import { salvar, deletar, pegaDados, persistirValores } from "./Banco.js";
 
-salvar("Flavio", 1);
-salvar("Ana", 2);
-salvar("Joao", 30);
-salvar("Maria", 41);
+// console.log(pegaDados());
+
+// salvar("Flavio");
+// salvar("Ana");
+// salvar("Joao");
+// salvar("Maria");
+
+// deletar(5);
+deletar(2);
+// deletar(7);
 
 console.log(pegaDados());
-
-deletar(41);
-deletar(30);
-
 console.log("---------------");
-
+salvar("Teste12", 7);
 console.log(pegaDados());
+persistirValores();
